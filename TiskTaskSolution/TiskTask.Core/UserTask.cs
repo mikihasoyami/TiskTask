a56461d45cf976b1acc04a5060bee6505d007399
elgrenka <bugigen@yandex.ru>
2025-08-05 22:08:12 +0400

    create class UserTask

---
+++ TiskTaskSolution/TiskTask.Core/UserTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiskTask.Core
+{
+    public class UserTask
+    {
+        public int Id { get; private set; }
+        public int UserId { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public DateTime Created { get; private set; }
+        public int TimeSpent {  get; private set; }
+    }
+}
