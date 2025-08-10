7edb1bddec5eb269c4e5678bff4adad8325369bb
Aleksei <aleksei.ojegov@yandex.ru>
2025-08-10 13:17:14 +0400

    Add files via upload

---
+++ TiskTask.Core/TelegramBotLibraryContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
+namespace TiskTask.Core;
+
+public partial class TelegramBotLibraryContext : DbContext
+{
+    public TelegramBotLibraryContext()
+    {
+    }
+
+    public TelegramBotLibraryContext(DbContextOptions<TelegramBotLibraryContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<UserTask> UserTasks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var dbPath = Path.Combine(AppContext.BaseDirectory, "libraryApp.db");
+        optionsBuilder.UseSqlite($"Data Source={dbPath}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UserTask>(entity =>
+        {
+            entity.ToTable("UserTask");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
